4735ee4920a1a3938d3d0b25470cfdcc40b8a81b
naoki <naokipgt@gmail.com>
2020-06-11 00:17:46 +0900

    issues/17 : リファクタ，コメント化されて使われなくなっていた部分を消した

--- p5js/p5save.js
+++ p5js/p5save.js
@@ -1,23 +1,17 @@
 const puppeteer = require('puppeteer');
+const dotenv = require('dotenv')
 
-// (async () => {
-//   const browser = await puppeteer.launch({
-//     executablePath: '/usr/bin/chromium-browser',
-//     headless: true,
-//   });
-//   const page = await browser.newPage();
-//   await page.goto('http://localhost:3000/p5js/');
-//   await page.screenshot({ path: './p5js/result.png', clip: {x: 0, y: 0, width: 600, height: 600} });
-//   await browser.close();
-// })();
+dotenv.config()
 
 exports.p5save = async() => {
+  const P5_URL = 'http://localhost:3000/p5js/'
+  const RESULT_IMAGE_PATH = './p5js/result.png'
   const browser = await puppeteer.launch({
-    executablePath: '/usr/bin/chromium-browser',
+    executablePath: `${process.env.CHROMIUM_PATH}`,
     headless: true,
   });
-  const page = await browser.newPage();
-  await page.goto('http://localhost:3000/p5js/');
-  await page.screenshot({ path: `./p5js/result.png`, clip: {x: 0, y: 0, width: 600, height: 600} });
-  await browser.close();
+  const page = await browser.newPage()
+  await page.goto(P5_URL)
+  await page.screenshot({ path: RESULT_IMAGE_PATH, clip: {x: 0, y: 0, width: 600, height: 600} })
+  await browser.close()
 }
--- twitter.js
+++ twitter.js
@@ -15,11 +15,58 @@ const client = new twitter({
     access_token_secret : `${process.env.ACCESS_TOKEN_SECRET}`,
 });
 
+// 各ツイートへのリプライ, 保存
+const action = async (tweet) => {
+  // リプライ先が #p5js を含むツイートか調べる
+  const mentionedTweet = await client.get('statuses/show', {
+    'id': tweet.in_reply_to_status_id_str,
+    'tweet_mode': 'extended',
+    'trim_user': true,
+    'include_entities': false,
+  })
+
+  if(mentionedTweet.full_text.indexOf('#p5js') === -1) return
+
+  // 一部記号をアンエスケープする
+  const fullText = mentionedTweet.full_text
+    .replace(/(&lt;)/g, '<')
+    .replace(/(&gt;)/g, '>')
+    .replace(/(&quot;)/g, '"')
+    .replace(/(&#39;)/g, "'")
+    .replace(/(&amp;)/g, '&');
+
+  // @メンションを消す
+  console.log(tweet.text)
+  const args = tweet.text.split(`@${user_data.screen_name} `).join('').split(' ')
+
+  // スクリプトの書き換えを行う
+  const modified = await modify(fullText, args)
+
+  // 画像の作成を行う
+  const res = await p5save()
+
+  const RESULT_PATH = './p5js/result.png'
+  // ツイート用画像のアップロードを行う
+  const image = await promisify(fs.readFile)(RESULT_PATH)
+  const uploadRes = await client.post('media/upload', {
+    'media': image
+  })
+
+  // 画像を使ってリプライを送る
+  await client.post('statuses/update', {
+    'status': `@${tweet.user.screen_name}`,
+    'in_reply_to_status_id': tweet.id_str,
+    'media_ids': uploadRes.media_id_string,
+  })
+
+}
+
+let user_data
+
 const main = async () => {
 
   // ユーザーデータ (主に検索用のスクリーンネーム) を取得する
-  const user_data = await client.get('account/verify_credentials', {})
-  // console.log(user_data)
+  user_data = await client.get('account/verify_credentials', {})
 
   // 自身に対するリプライを取得する
   const tweets = await client.get('search/tweets', {
@@ -27,85 +74,24 @@ const main = async () => {
   })
 
   // リプライ履歴の確認
-  const filePath = './tweet_data/tweet.csv'
-  const writeFileResult = await promisify(fs.appendFile)(filePath,'')
-  const readResult = await promisify(fs.readFile)(filePath, 'utf-8')
+  const HISTORY_PATH = './tweet_data/tweet.csv'
+  const writeFileResult = await promisify(fs.appendFile)(HISTORY_PATH,'')
+  const readResult = await promisify(fs.readFile)(HISTORY_PATH, 'utf-8')
   const memoryTweetIds = readResult.split(',')
-
-  // 各ツイートへのリプライ, 保存
-  const action = async (tweet) => {
-    if(memoryTweetIds.findIndex(id => id === tweet.id_str) === -1) {
-      const out = await promisify(fs.appendFile)(filePath, `${tweet.id_str},`)
-
-      // ここに初めて観測したリプライに対して行う処理を書く
-      // リプライ先が #p5js を含むツイートか調べる
-      const mentionedTweet = await client.get('statuses/show', {
-        'id': tweet.in_reply_to_status_id_str,
-        'tweet_mode': 'extended',
-        'trim_user': true,
-        'include_entities': false,
-      })
-
-      if(mentionedTweet.full_text.indexOf('#p5js') !== -1){
-        // 一部記号をアンエスケープする
-        const fullText = mentionedTweet.full_text
-          .replace(/(&lt;)/g, '<')
-          .replace(/(&gt;)/g, '>')
-          .replace(/(&quot;)/g, '"')
-          .replace(/(&#39;)/g, "'")
-          .replace(/(&amp;)/g, '&');
-
-        // @メンションを消す
-        console.log(tweet.text)
-        const args = tweet.text.split(`@${user_data.screen_name} `).join('').split(' ')
-
-        // スクリプトの書き換えを行う
-        // const modified = await modify(fullText, tweet.text.split(' '))
-        const modified = await modify(fullText, args)
-
-        // 画像の作成を行う
-        const res = await p5save()
-
-        // ツイート用画像のアップロードを行う
-        const image = await promisify(fs.readFile)('./p5js/result.png')
-        const uploadRes = await client.post('media/upload', {
-          'media': image
-        })
-
-        await client.post('statuses/update', {
-          'status': `@${tweet.user.screen_name}`,
-          'in_reply_to_status_id': tweet.id_str,
-          'media_ids': uploadRes.media_id_string,
-        })
-
-        // 画像を使ってリプライを送る
-
-      }
-      // await client.post('statuses/update', {
-      //   'status': 'test tweet',
-      //   'in_reply_to_status_id': tweet.id_str,
-      // })
-    }
-  }
 
   // 検索した全ツイートへのリプライ
   // TODO: リプライ済みのツイートか調べる処理が必要
   for(tweet of tweets.statuses){
+    if(memoryTweetIds.findIndex(id => id === tweet.id_str) !== -1) continue
+    const out = await promisify(fs.appendFile)(filePath, `${tweet.id_str},`)
     await action(tweet)
   }
-  // const allReply= async () => {
-  //   await Promise.all(tweets.statuses.map(async tweet => await action(tweet)))
-  // }
-  // allReply()
 }
-
-// main()
 
 const sleep = msec => new Promise(resolve => setTimeout(resolve, msec));
 
 (async () => {
   while(true){
-    console.log('call')
     main()
     await sleep(1000*60)
   }
