1e23172d4f7892770f393bb3efda7fbc4520444d
naoki <naokipgt@gmail.com>
2020-06-15 00:48:58 +0900

    issues/21 : 複数使用しているパスの共通化をした

--- p5js/p5save-movie.js
+++ p5js/p5save-movie.js
@@ -8,15 +8,15 @@ dotenv.config()
 exports.p5saveMovie = async() => {
   const P5_URL = 'http://localhost:3000/p5js/movie.html'
   const RESULT_IMAGE_PATH = './p5js/result.png'
+  const MOVIE_DIR = './p5js/movie'
   const browser = await puppeteer.launch({
     executablePath: `${process.env.CHROMIUM_PATH}`,
     headless: true,
   });
   const page = await browser.newPage()
-  const downloadPath = './p5js/movie/'
   await page._client.send(
     'Page.setDownloadBehavior',
-    { behavior: 'allow', downloadPath: downloadPath }
+    { behavior: 'allow', downloadPath: MOVIE_DIR}
   );
   await page.goto(P5_URL)
   console.log('capture start')
@@ -26,12 +26,12 @@ exports.p5saveMovie = async() => {
 
   // gif化する
   console.log('start convert gif')
-  let time = await promisify(exec)('time convert -delay 12 -loop 0 ./p5js/movie/frame_*.png ./p5js/movie/dst.gif').catch((err) => {
+  let time = await promisify(exec)(`time convert -delay 12 -loop 0 ${MOVIE_DIR}/frame_*.png ${MOVIE_DIR}/dst.gif`).catch((err) => {
     console.log(err)
   })
   console.log('finish convert gif')
   console.log(time)
   console.log('remove buffer file')
-  let removeRes = await promisify(exec)('rm ./p5js/movie/*.png')
+  let removeRes = await promisify(exec)(`rm ${MOVIE_DIR}/*.png`)
   console.log('finish remove buffer file')
 }
